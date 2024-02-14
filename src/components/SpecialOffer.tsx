021417f942df59bed84b97a528b221aa767656a2
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2024-02-14 16:52:50 +0300

    my changes

--- src/components/SpecialOffer.tsx
+++ src/components/SpecialOffer.tsx
@@ -1,9 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
 function SpecialOffer() {
   return (
-    <div>
-      SPECIAL OFFER
+    <div >
+
+    <div className='flex w-full relative h-[50vh] bg-[navajowhite]'>
+      <p>Special Offer</p>
+      <p>50% OFF</p>
+      <p>Rush while it still lasts</p>
+    </div>
+
+    <div className='grid w-full h-[50vh]'>
+    <div className='flex'>
+    <Image src="/images/sisterlocs1.png" alt="Special Offer" layout="fill" objectFit="cover" />
+    <Image src="/images/sisterlocs1.png" alt="Special Offer" layout="fill" objectFit="cover" />
+    </div>
+    <div className='flex'>
+    <Image src="/images/sisterlocs1.png" alt="Special Offer" layout="fill" objectFit="cover" />
+    <Image src="/images/sisterlocs1.png" alt="Special Offer" layout="fill" objectFit="cover" />
+    </div>
+    </div>
+
     </div>
   )
 }
