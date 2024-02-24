284d45deeb98339526bc111f3a353e448927cbed
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2024-02-24 11:54:39 +0300

    remove animation on title

--- src/components/commons/Title.tsx
+++ src/components/commons/Title.tsx
@@ -5,14 +5,14 @@ import SlideAnimation from './SlideAnimation';
   }
 function Title({title}:TitleProps) {
   return (
-  <SlideAnimation direction='right' >
+  <div >
 <div className='py-5  grid  justify-center'>
 <div className='text-[20px] md:text-[30px] text-black_1  capitalize font-semibold  font-[System]'>
       {title}
     </div>
      <div className='h-[3px] md:h-1 w-[30px] md:w-[50px]  bg-gradient-to-r from-brown_1  via-[#FFAF00]  to-[#FFAF00] '></div>
 </div>
-  </SlideAnimation>
+  </div>
   )
 }
 
